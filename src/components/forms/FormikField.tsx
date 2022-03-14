6014b9f6c8a7c28ae35957000005aafa6cc28730
Andreas Tarakanov <andy_tarakanov@web.de>
2022-03-14 14:30:58 +0000

    refactoring

--- src/components/forms/FormikField.tsx
+++ src/components/forms/FormikField.tsx
@@ -4,18 +4,19 @@ import {
   FormLabel,
   Input,
 } from "@chakra-ui/react";
-import { Field, FieldAttributes } from "formik";
+import { Field, FieldProps } from "formik";
+import { Contact } from "../../types/Contact";
 
 type FormikFieldProps = {
-  name: string;
+  name: keyof Contact;
   label: string;
 };
 
 const FormikField = ({ name, label }: FormikFieldProps) => {
   return (
     <Field name={name}>
-      {({ field, form }: FieldAttributes<any>) => (
-        <FormControl isInvalid={form.errors[name] && form.touched[name]}>
+      {({ field, form }: FieldProps<typeof name, Contact>) => (
+        <FormControl isInvalid={!!form.errors[name] && form.touched[name]}>
           <FormLabel htmlFor={name}>{label}</FormLabel>
           <Input {...field} id={name} placeholder={label} />
           <FormErrorMessage>{form.errors[name]}</FormErrorMessage>
